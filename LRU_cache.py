db3a904ac8e5d6e8b9b367ef1de55179a09e0dff
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-04-16 23:49:31 +0530

    Added Methods for LRU

--- LRU_cache.py
+++ LRU_cache.py
@@ -1,6 +1,15 @@
 class LRU:
+
     def __init__(self):
         self.lrucache = []
         self.size = 6
-
+    
+    def put(self,item):
+        pass
+    
+    def get(self):
+        pass
+    
+    def get_cache(self):
+        pass
     
