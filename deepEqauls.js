50421f14be751ce63b3eb7ef6cc6d2b60d859f27
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-04-29 23:34:52 +0530

    created recursion and checked the length of the two values

--- deepEqauls.js
+++ deepEqauls.js
@@ -23,6 +23,14 @@ function isObject(obj) {
 function deepEqual(val1, val2) {
     if(val1 == val2) {
         return true;
+    } else if (isObject(val1) && isObject(val2)) {
+        if(Object.keys(val1).length != Object.keys(val2).length) return false;
+        for(let i in obj1) {
+            if(!deepEqual(val1[i], val2[i])) {
+                return false;
+            }
+        }
+        return true;
     } else {
         return false;
     }
