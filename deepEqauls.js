f4ebf2837aac3740c8177b76f704a185a1725727
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-04-29 23:31:49 +0530

    created function deepEqual to check the valus are equal or not

---
+++ deepEqauls.js
@@ -0,0 +1,26 @@
+//created two objects 
+const obj1 = {
+     name: 'javascript',
+     n: 'js'
+}
+const obj2 = {
+    name: 'javascript',
+    n: 'js'
+}
+
+// const obj2 = Object.assign({}.obj1)
+
+
+//created a function with two parameters for checking the values
+function deepEqual(val1, val2) {
+    if(val1 == val2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//different conditions to check deepEqual function 
+console.log(deepEqual(obj1, obj2))
+console.log(deepEqual(1, 2))
+console.log(deepEqual(1,"1"))
