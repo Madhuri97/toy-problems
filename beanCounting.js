5b87928934eaaf304bf0a8263487d78942cdc980
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-04-29 23:27:28 +0530

    created countChar method with two parameters, one is string and other as letter

--- beanCounting.js
+++ beanCounting.js
@@ -13,4 +13,20 @@ const countBs = function(s) {
   }; 
   console.log("Count of B's in given String is: ", countBs("Blinking Blindly, Brainy Bob Brings Beautiful Beer Bottles Beneath Blue Butterflies Billowing Brilliantly."));
 
+  console.log("----------------------------------------------")
   
+//write a function called countChar that behaves 
+// like countBs, except it takes a second argument 
+// that indicates the character that is to be counted 
+// (rather than counting only uppercase “B” characters). 
+// Rewrite countBs to make use of this new function.
+function countChar(s,l) {
+    let count = 0;
+    for(let i = 0; i < s.length; i++) {
+        if (s.charAt(i) == l) {
+            count = count +1;
+        }
+    }
+    return count;
+}
+console.log("Count of given letter in the given string is", countChar("Blinking Blindly, Brainy Bob Brings Beautiful Beer Bottles Beneath Blue Butterflies Billowing Brilliantly.",'i'));
